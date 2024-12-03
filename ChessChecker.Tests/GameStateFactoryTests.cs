0000d014a86d44c67b5fd90fc062c100d8e727c1
gyurka66 <nygyurka@gmail.com>
2024-12-03 16:32:11 +0100

    Add GameStateFactory Test Class

---
+++ ChessChecker.Tests/GameStateFactoryTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessChecker.Tests
+{
+    [TestFixture]
+    public class GameStateFactoryTests
+    {
+        /* I just realized we don't need this yet
+        public void GameStateShouldBeCreated()
+        {
+            GameStateFactory factory = new GameStateFactory();
+            GameState state = factory.CreateGameState("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR");
+
+        }
+        */
+    }
+}
--- ChessChecker/Bishop.cs
+++ ChessChecker/Bishop.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ChessChecker
 {
-    internal class Bishop
+    public class Bishop
     {
     }
 }
