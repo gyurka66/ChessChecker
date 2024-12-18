29b3f7eebf38dc1c9b6b37c5f273fd5942dc9d83
Aiden Sheehan <as227mj@student.lnu.se>
2024-12-18 15:33:03 +0000

    [RED] FENString - ShouldFillPiecePlacementOnValidInput

--- ChessChecker.Tests/FENStringTests.cs
+++ ChessChecker.Tests/FENStringTests.cs
@@ -16,5 +16,18 @@ namespace ChessChecker.Tests
         {
             Assert.Throws<ArgumentException>(() => new FENString(input));
         }
+
+        [TestCase("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w - - 0 0")]
+        [TestCase("4k2r/6r1/8/8/8/8/3R4/R3K3 w Qk - 0 1")]
+        [TestCase("rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1")]
+        public void ShouldFillPiecePlacementOnValidInput(string input)
+        {
+            var actual = new FENString(input);
+            Assert.Multiple(() =>
+            {
+                Assert.That(actual.piecePlacement, Is.Not.Null);
+                Assert.That(actual.piecePlacement, Is.Not.Empty);
+            });
+        }
     }
 }
