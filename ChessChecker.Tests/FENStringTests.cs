70be69cfcb5813ff223a3c95b24d4eda8af83972
Aiden Sheehan <as227mj@student.lnu.se>
2024-12-18 15:41:33 +0000

    [RED] FENString - ShouldFillActiveColourOnValidInput

--- ChessChecker.Tests/FENStringTests.cs
+++ ChessChecker.Tests/FENStringTests.cs
@@ -29,5 +29,18 @@ namespace ChessChecker.Tests
                 Assert.That(actual.PiecePlacement, Is.Not.Empty);
             });
         }
+
+        [TestCase("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w - - 0 0")]
+        [TestCase("4k2r/6r1/8/8/8/8/3R4/R3K3 w Qk - 0 1")]
+        [TestCase("rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1")]
+        public void ShouldFillActiveColourOnValidInput(string input)
+        {
+            var actual = new FENString(input);
+            Assert.Multiple(() =>
+            {
+                Assert.That(actual.ActiveColour, Is.Not.Null);
+                Assert.That(actual.ActiveColour, Is.Not.Empty);
+            });
+        }
     }
 }
