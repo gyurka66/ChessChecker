36768ab56f7e535df73b96707a3c984f5120821e
Aiden Sheehan <as227mj@student.lnu.se>
2024-12-18 16:42:48 +0000

    [RED] FENString - ShouldThrownExcpetionOnMissingField

--- ChessChecker.Tests/FENStringTests.cs
+++ ChessChecker.Tests/FENStringTests.cs
@@ -30,6 +30,18 @@ namespace ChessChecker.Tests
             });
         }
 
+        [TestCase("w - - 0 0")]
+        [TestCase("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR - - 0 0")]
+        [TestCase("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w - 0 0")]
+        [TestCase("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w 0 0")]
+        [TestCase("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w - - 0")]
+        [TestCase("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w - -")]
+        [TestCase("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR")]
+        public void ShouldThrowExceptionOnMissingField(string input)
+        {
+            Assert.Throws<ArgumentException>(() => new FENString(input));
+        }
+
         [TestCase("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w - - 0 0")]
         [TestCase("4k2r/6r1/8/8/8/8/3R4/R3K3 w Qk - 0 1")]
         [TestCase("rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1")]
