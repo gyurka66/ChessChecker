0da21f5ee4a6d6d8f685fab84c7e038131bac6a8
Aiden Sheehan <as227mj@student.lnu.se>
2024-12-18 15:14:41 +0000

    Added chess fields

--- ChessChecker/FENString.cs
+++ ChessChecker/FENString.cs
@@ -5,8 +5,19 @@ using System.Threading.Tasks;
 
 namespace ChessChecker
 {
+    /// <summary>
+    /// String used to encode the current state of a chess game.
+    /// https://www.chess.com/terms/fen-chess
+    /// </summary>
     public class FENString
     {
+        public readonly string piecePlacement;
+        public readonly string activeColour;
+        public readonly string castlingRights;
+        public readonly string enPassentTargets;
+        public readonly string halfmoveClock;
+
+        public readonly string fullmoveNumber;
         public FENString(string input)
         {
             if (string.IsNullOrWhiteSpace(input))
