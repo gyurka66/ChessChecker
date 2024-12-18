9de5d7f7ae1ec8b955c9a7317fb0b2f865deae30
Aiden Sheehan <as227mj@student.lnu.se>
2024-12-18 17:30:27 +0000

    [GREEN] Made FENString - ShouldFillActiveColourOnValidInput more robust

--- ChessChecker.Tests/FENStringTests.cs
+++ ChessChecker.Tests/FENStringTests.cs
@@ -46,17 +46,15 @@ namespace ChessChecker.Tests
             Assert.Throws<ArgumentException>(() => new FENString(input));
         }
 
-        [TestCase("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w - - 0 0")]
-        [TestCase("4k2r/6r1/8/8/8/8/3R4/R3K3 w Qk - 0 1")]
-        [TestCase("rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1")]
-        public void ShouldFillActiveColourOnValidInput(string input)
+        [TestCase("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w - - 0 0", ExpectedResult = "w")]
+        [TestCase("4k2r/6r1/8/8/8/8/3R4/R3K3 w Qk - 0 1", ExpectedResult = "w")]
+        [TestCase(
+            "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1",
+            ExpectedResult = "b"
+        )]
+        public string ShouldFillActiveColourOnValidInput(string input)
         {
-            var actual = new FENString(input);
-            Assert.Multiple(() =>
-            {
-                Assert.That(actual.ActiveColour, Is.Not.Null);
-                Assert.That(actual.ActiveColour, Is.Not.Empty);
-            });
+            return new FENString(input).ActiveColour;
         }
     }
 }
