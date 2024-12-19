0468fee1e2e1360de12308107b8c346e1bd0d81c
Aiden Sheehan <as227mj@student.lnu.se>
2024-12-19 01:04:58 +0000

    [Red] Square- ShouldThrowExceptionWhenTryingToReplacePiece

--- ChessChecker.Tests/PieceTests/SquareTests.cs
+++ ChessChecker.Tests/PieceTests/SquareTests.cs
@@ -9,13 +9,16 @@ namespace ChessChecker.Tests.PieceTests
     [TestFixture]
     public class SquareTests
     {
-        private Piece? _mockPiece;
+        private Piece? _mockPieceA;
+        private Piece? _mockPieceB;
 
         [SetUp]
         public void Setup()
         {
-            _mockPiece = Substitute.For<Piece>();
-            _mockPiece.Color.Returns(IPiece.PlayerColor.White);
+            _mockPieceA = Substitute.For<Piece>();
+            _mockPieceA.Color.Returns(IPiece.PlayerColor.White);
+            _mockPieceB = Substitute.For<Piece>();
+            _mockPieceB.Color.Returns(IPiece.PlayerColor.Black);
         }
 
         [Test]
@@ -28,8 +31,15 @@ namespace ChessChecker.Tests.PieceTests
         [Test]
         public void ShouldConstructWithRealPiece()
         {
-            Square square = new(_mockPiece);
-            Assert.That(square.Piece, Is.EqualTo(_mockPiece));
+            Square square = new(_mockPieceA);
+            Assert.That(square.Piece, Is.EqualTo(_mockPieceA));
+        }
+
+        [Test]
+        public void ShouldThrowExceptionWhenTryingToReplacePiece()
+        {
+            Square square = new(_mockPieceA);
+            Assert.Throws<ApplicationException>(() => square.Piece = _mockPieceB);
         }
     }
 }
