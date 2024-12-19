e911525dbad248a0255835aea44e77bcdeda595b
Aiden Sheehan <as227mj@student.lnu.se>
2024-12-19 12:51:48 +0000

    [Green] GameState - AddToSquareShouldThrowExceptionOnNullPiece

--- ChessChecker/GameState.cs
+++ ChessChecker/GameState.cs
@@ -20,5 +20,13 @@ namespace ChessChecker
             _board = board ?? throw new NullReferenceException("Supplied an empty board array");
             _activeColour = color;
         }
+
+        public void AddToSquare(IPiece piece, (int f, int r) position)
+        {
+            if (piece == null)
+            {
+                throw new ArgumentException("Can't add a null piece");
+            }
+        }
     }
 }
