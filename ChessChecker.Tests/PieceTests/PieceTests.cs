94378abec524515553edbcf43653b5b73938fa00
Aiden Sheehan <as227mj@student.lnu.se>
2024-12-19 00:26:44 +0000

    [Red] Added colour tests for piece

---
+++ ChessChecker.Tests/PieceTests/PieceTests.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChessChecker.Tests.PieceTests
+{
+    [TestFixture]
+    public class PieceTests
+    {
+        private class MockPiece(IPiece.PlayerColor col) : Piece(col)
+        {
+            public override List<Square> GetPossibleMoves(Square[,] board, (int, int) piecePosition)
+            {
+                return [];
+            }
+        }
+
+        [Test]
+        public void ShouldBeWhite()
+        {
+            Piece piece = new MockPiece(IPiece.PlayerColor.White);
+            Assert.That(piece.Color, Is.EqualTo(IPiece.PlayerColor.White));
+        }
+
+        [Test]
+        public void ShouldBeBlack()
+        {
+            Piece piece = new MockPiece(IPiece.PlayerColor.Black);
+            Assert.That(piece.Color, Is.EqualTo(IPiece.PlayerColor.Black));
+        }
+    }
+}
